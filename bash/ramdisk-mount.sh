ce4921e2507fdf6f79f77064574b3599e7937d7b
Michael <RedSQL@users.noreply.github.com>
2020-06-10 17:26:38 +0000

    Fix unmounting

--- bash/ramdisk-mount.sh
+++ bash/ramdisk-mount.sh
@@ -7,7 +7,7 @@ MBRAMAMOUNT=$(kdialog --inputbox "Enter the size your ramdisk should be. (i.e 20
 if [ $MBRAMAMOUNT -eq 0 ]
 then
     kdialog --msgbox "Number supplied was 0. Unmounting ramdisk."
-    sudo -A umount PATH_TO_RAMDISK
+    sudo -A umount $PATH_TO_RAMDISK
 else
     # I am well aware that cancelling input box above will cause this to execute. I'm just too lazy to implement the check. (Want to do that yourself? PRs are always welcome!) Nothing catastrophic happens if you cancel out, anyway.
     sudo -A mount -t tmpfs -o size=$MBRAMAMOUNT $RAMDISK_NAME $PATH_TO_RAMDISK
