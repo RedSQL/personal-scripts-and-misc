91706fffc3331a2738d2d00e69a11356bda97875
Michael <RedSQL@users.noreply.github.com>
2020-07-03 19:54:31 +0000

    :-) I hate userscripts
    We down to 1 issue, now the style may appear for a split second before going modded.

--- userstyles/BetterNewGithub/BetterNewGithub.user.js
+++ userstyles/BetterNewGithub/BetterNewGithub.user.js
@@ -1,40 +1,16 @@
 // ==UserScript==
-// @name        Better New Github (UserScript Edition)
+// @name        Better New Github (UserScript Edition2)
 // @namespace   github.com/RedSQL/personal-scripts-and-misc
 // @match       https://github.com/*
-// @grant       none
+// @grant       GM_addStyle
 // @license     MIT (But see for the latest license here: github.com/RedSQL/personal-scripts-and-misc/blob/master/LICENSE)
-// @version     0.0.1
+// @version     0.0.2
 // @author      RedEclipse (RedSQL)
 // @description Why, Microsoft? If it ain't broke - don't fix it. 
 // ==/UserScript==
 ;(function () {
   'use strict'
   window.addEventListener('load', function() {
-  var elems_array = ['container-xl','container-lg','avatar-user','user-status-circle-badge-container'];
-  for (var x in elems_array) {
-    var elem = document.getElementsByClassName(elems_array[x]);
-    for (var i in elem){
-      if (elem.hasOwnProperty(i)) {
-        console.log(elem[i]);
-        switch(elems_array[x]) {
-          case 'container-lg':
-            elem[i].style.maxWidth = '1800px';
-            break;
-          case 'container-xl':
-            elem[i].style.maxWidth = '1800px';
-            break;
-          case 'avatar-user':
-            elem[i].style.borderRadius = '5%';
-            break;
-          case 'user-status-circle-badge-container':
-            elem[i].style.marginBottom = '0px';
-            break;
-          default:
-            console.log("Nothing to do.")
-        }
-      }
-    }
-  }
+    GM_addStyle(".container-xl{max-width:1800px!important}.container-lg{max-width:1800px!important}.avatar-user{border-radius:5%!important}.user-status-circle-badge-container{margin-bottom:0!important}");
   }, false);
 })();
