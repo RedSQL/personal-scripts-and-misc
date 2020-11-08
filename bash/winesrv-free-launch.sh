7a1aeb8205d06b925c6c684ef70372c1a1db2fe5
Michael <RedSQL@users.noreply.github.com>
2020-11-08 03:29:31 +0000

    Update winesrv-free-launch.sh
    Okay, I can explain this one a little bit...
    
    *ln46 exists in case I launch the app again with a shortcut while the previous instance of that application running is getting shut down.
    *I made a function with the entire stuff in it to call when needed to launch wine, it comes with an arg supplier which if passed, would re-detect wineserver and the application name and promptly kill it
    
    I will make this better... at some point... One day... Maybe...

--- bash/winesrv-free-launch.sh
+++ bash/winesrv-free-launch.sh
@@ -1,9 +1,58 @@
 #!/usr/bin/bash
+# Constants
+
+# Workaround because I cannot detect wine application with pgrep :/
+PROC_NAME=""
+WINE_EXECUTABLE=$(ps -A | grep $PROC_NAME | awk '{print $4}')
+
+# Options
+
+# Enables Konsole for log output
+with_console=1
+
+# Custom Exports
+export WINEESYNC=1
+export WINEFSYNC=1
+
+# Function definition
+
+# Pass any 1st argument in to re-detect wineserver and application
+launch_wine() {
+    if [ $1 ]; then
+        if pgrep -x "wineserver" >/dev/null; then
+            if [ -z "$WINE_EXECUTABLE" ]; then
+                kdesu sudo killall wineserver
+                kdesu sudo killall $PROC_NAME
+                sleep 5
+                launch_wine
+            else
+                launch_wine
+            fi
+        fi
+    fi
+    # Your wine launch code below!
+    if [ $with_console == 1 ]; then
+        # konsole -e YOUR WINE LAUNCH CODE HERE!
+    else
+        # YOUR WINE LAUNCH CODE HERE!
+    fi
+    
+}
+
+# Main launch
 if pgrep -x "wineserver" >/dev/null; then
-    if kdialog --title "Wineserver detected!" --yesno "You have winesrv running! Do you want to kill it?"; then
-        kdesu sudo killall wineserver
+    # Seeing if wineserver is still active after a while
+    sleep 7
+    if pgrep -x "wineserver" >/dev/null; then
+        if kdialog --title "Wineserver detected!" --yesno "You have wineserver running! Do you want to kill it?"; then
+            kdesu sudo killall wineserver
+            sleep 3
+            launch_wine 1
+        fi
+    else
+        launch_wine 1
     fi
 else
-    echo "Wineserver is not running!"
-    # Your custom Wine launch code
+    echo "Wineserver is not running! Proceeding."
+    launch_wine
 fi
