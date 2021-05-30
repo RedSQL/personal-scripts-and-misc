16274105624d638ea710ab4c2555c3d98fb70e28
Mike <RedSQL@users.noreply.github.com>
2021-05-30 13:31:03 +0000

    Update tsremover.user.js
    Ah, right, forgot to increment version since... first version. Oops.

--- userscripts/Youtube URL Timestamp Remover/tsremover.user.js
+++ userscripts/Youtube URL Timestamp Remover/tsremover.user.js
@@ -4,7 +4,7 @@
 // @homepageURL https://github.com/RedSQL/personal-scripts-and-misc/
 // @match       https://www.youtube.com/*
 // @grant       none
-// @version     1.0.0
+// @version     1.0.4
 // @author      RedSQL (RedEclipse)
 // @description Removes &t=num from the URL on youtube videos so youtube has to pull latest timestamp elsewhere.
 // @license     MIT (But see for the latest license here: github.com/RedSQL/personal-scripts-and-misc/blob/master/LICENSE)
