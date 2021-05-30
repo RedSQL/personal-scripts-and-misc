19ae2f76bc26a8f0a5e087997f425f05ccf4ff1f
Mike <RedSQL@users.noreply.github.com>
2021-05-30 13:29:36 +0000

    Update tsremover.user.js
    I couldn't for the life of me figure out how to fire an event during url change, so I did with a dirty workaround - calling url clean function every 1 second. From what I've noticed, that did not degrade performance at all (on my end), even with like 30 and 40 youtube tabs open at the same time. 
    
    Included run-at attribute to make the script fire as document starts.
    
    Moved link cleaning into a function if I need to call it again.
    
    Added check for "feature" in the url, since "feature" usually appears when you clicked on a user link (for example a video with timestamp for something), and therefore I don't see the need to clean the link. This might trigger anyway if "feature" goes away from the url and the function is ran, but I'll solve that at some point later.

--- userscripts/Youtube URL Timestamp Remover/tsremover.user.js
+++ userscripts/Youtube URL Timestamp Remover/tsremover.user.js
@@ -8,12 +8,18 @@
 // @author      RedSQL (RedEclipse)
 // @description Removes &t=num from the URL on youtube videos so youtube has to pull latest timestamp elsewhere.
 // @license     MIT (But see for the latest license here: github.com/RedSQL/personal-scripts-and-misc/blob/master/LICENSE)
+// @run-at      document-start
 // ==/UserScript==
 ;(() => {
   'use strict'
-    if(location.search.includes("&t=") && location.pathname == "/watch") {
+  function clearurlfunc() {  
+    if(location.search.includes("&t=") && location.pathname == "/watch" && !location.search.includes("feature")) {
       let cleanurl = location.href.split("&");
       cleanurl.splice(cleanurl.indexOf('t='), 1);
       location.replace(cleanurl.join("&"));
     }
+  setInterval(function() {
+    clearurlfunc();
+  }, 1000);
+  clearurlfunc();
 })();
