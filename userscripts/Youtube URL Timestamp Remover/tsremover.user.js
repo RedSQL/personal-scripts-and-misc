b9a53f7e751684f90d20601800fa2949803593a3
Mike <RedSQL@users.noreply.github.com>
2021-06-02 19:12:26 +0000

    Update tsremover.user.js
    Now doesn't need to run every second as a hacky workaround. Thanks to @jas0n098

--- userscripts/Youtube URL Timestamp Remover/tsremover.user.js
+++ userscripts/Youtube URL Timestamp Remover/tsremover.user.js
@@ -4,23 +4,31 @@
 // @homepageURL https://github.com/RedSQL/personal-scripts-and-misc/
 // @match       https://www.youtube.com/*
 // @grant       none
-// @version     1.0.4
+// @version     1.0.5
 // @author      RedSQL (RedEclipse)
 // @description Removes &t=num from the URL on youtube videos so youtube has to pull latest timestamp elsewhere.
 // @license     MIT (But see for the latest license here: github.com/RedSQL/personal-scripts-and-misc/blob/master/LICENSE)
 // @run-at      document-start
 // ==/UserScript==
+// Non-working timestamp remover - should have triggered on click but does not work when clicked on links.
 ;(() => {
   'use strict'
+  var lasturl = location.search;
   function clearurlfunc() {  
     if(location.search.includes("&t=") && location.pathname == "/watch" && !location.search.includes("feature")) {
       let cleanurl = location.href.split("&");
       cleanurl.splice(cleanurl.indexOf('t='), 1);
       location.replace(cleanurl.join("&"));
+      lasturl = location.search;
     }
   }
-  setInterval(function() {
-    clearurlfunc();
-  }, 1000);
+  // Thanks @jas0n098
+  document.addEventListener("mousedown", function() {
+    setTimeout(function(){
+      if(lasturl != location.search) {
+        clearurlfunc();
+      } 
+    }, 200);
+  });
   clearurlfunc();
 })();
