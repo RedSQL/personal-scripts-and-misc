83ef627c7e3cf873a92aa39a9540ded799da2082
Mike <RedSQL@users.noreply.github.com>
2021-12-09 20:08:09 +0000

    Wayland clarification
    Add a clarification for using clicker script on Wayland sessions.

--- bash/ac-rmb.sh
+++ bash/ac-rmb.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 # dirty autoclicker hack
+# FYI: This will NOT work on Wayland. This will only work on native X11 session or Xwayland windows.
+# Currently, Wayland does not have any way to capture keyboard keys through any API.
 QUERY_STATE=
 KEY_ID=
 while true; do
