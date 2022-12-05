921ed483f00a2ea0751b322a12611d4bba344d66
Mike <RedSQL@users.noreply.github.com>
2022-12-05 19:37:05 +0000

    Update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -7,5 +7,13 @@ function shchk_noargs
          end
 end
 
+function notify_func
+	$argv && kdialog --passivepopup 'Command finished successfully!' 10 || kdialog --passivepopup 'Command did NOT finish successfully! \nCheck terminal for details.' 30
+end
+
 # Defaults to check *all* shell scripts within the directory if no argument is supplied.
 alias shellcheck=shchk_noargs
+
+# Syntax: `notify <command>`
+# Notifies whether or not command finished successfully (returned 0) or not (returned >0)
+alias notify=notify_func
