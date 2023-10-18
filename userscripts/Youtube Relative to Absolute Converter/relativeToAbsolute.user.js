ece91d24e1e64ee9aa62d3c9b473cdf6ad817af9
Mike <RedSQL@users.noreply.github.com>
2023-10-18 20:24:34 +0000

    userscripts/relativeToAbsolute: Properly fix for new UI
    I don't need to do any of this mess when I can just override textContent of the info container!

--- userscripts/Youtube Relative to Absolute Converter/relativeToAbsolute.user.js
+++ userscripts/Youtube Relative to Absolute Converter/relativeToAbsolute.user.js
@@ -4,7 +4,7 @@
 // @homepageURL https://github.com/RedSQL/personal-scripts-and-misc/
 // @match       https://www.youtube.com/*
 // @grant       none
-// @version     1.0.7
+// @version     1.0.8
 // @author      RedSQL (RedEclipse)
 // @description Replaces relative video timestamp from the video description to absolute.
 // @license     https://github.com/RedSQL/personal-scripts-and-misc/blob/master/LICENSE
@@ -22,9 +22,7 @@
           date_absolute = document.querySelectorAll('tp-yt-paper-tooltip.style-scope.ytd-watch-info-text')[0].outerText.trim();
         }
         if(typeof(desc_inner) != 'undefined' && desc_inner != null && date_absolute != null) {
-          desc_inner.children[0].textContent = "";
-          desc_inner.children[2].textContent = "";
-          desc_inner.children[1].textContent = date_absolute;
+          desc_inner.textContent = date_absolute;
           desc_inner.children[1].style.setProperty("font-weight",500);
           clearInterval(pageLoadWait);
         } else {
